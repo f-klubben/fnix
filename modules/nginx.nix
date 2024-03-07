de61fb1e8ce980c02f4fb218dc9f9c7f7720ce06
mast3r <thomasmollerjensen@live.dk>
2024-03-07 14:47:43 +0100

    changed name of index file

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -22,7 +22,7 @@ in {
             # it will only run on localhost, its for debugging
             "routing.localhost" = {
                 listen = [{ port = 80; addr = "localhost"; }];
-                locations."/".index = "${pkgs.writeText "/var/fit/index.html" ''
+                locations."/".index = "${pkgs.writeText "index.html" ''
                     <div style="text-align: center;">
                         <h3>F-Klubben | Routing</h3>
                         <a href="http://${portmap.domain}/">Fikien</a><br>
