ecb81c32e0e0decfd181ddc8ec5f0e713dc5a676
Mast3r_waf1z <thomasmollerjensen@live.dk>
2024-03-08 01:25:35 +0100

    moved packages

--- system/users.nix
+++ system/users.nix
@@ -16,9 +16,5 @@ in {
         description = "Thomas";
         extraGroups = [ "wheel" ];
         hashedPassword = "$6$rounds=2000000$htFKKf65jcKCw09Z$JNmYnL5lIBZP6dvqYXUmj0vzzaiRteXOwlJzkcYcRCYdT5Zt8TVJWvtT4w4Q8suBneVOLEjxsMIf0yEY4BDrz1";
-        packages = with pkgs; [
-            lynx
-            vim
-        ];
     };
 }
